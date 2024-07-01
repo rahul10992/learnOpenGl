2ff5091f172fedbc1d13b0792e343a2eccb3b83b
rahul <10383453+rahul10992@users.noreply.github.com>
2024-07-01 07:30:12 -0700

    hello window done

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,74 @@
 #include "config.h"
 // https://www.youtube.com/watch?v=4m9RHfdUU_M
-int main() {
-    
-    GLFWwindow* window;
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main() {
+    // glfw: initialize and configure
+    // ------------------------------
     if(!glfwInit()){
         std::cout << "GLFW couldn't be loaded" << std::endl;
         return -1;
     }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    window = glfwCreateWindow(640, 480, "Hello openGL", NULL, NULL);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
     glfwMakeContextCurrent(window);
+    // allow for resizing
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // glad: load all OpenGL function pointers
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
         std::cout << "GLAD couldn't be loaded" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
-    while (!glfwWindowShouldClose(window))
+    // render loop
+    while(!glfwWindowShouldClose(window))
     {
-        glfwPollEvents();
+        // input
+        processInput(window);
+
+        // rendering code goes here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // poll all events and swap buffers
+        glfwPollEvents();
         glfwSwapBuffers(window);
     }
-    
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
     glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
