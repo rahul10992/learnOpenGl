34a16c0b611660a17837d2ef318148944f4648a2
rahul <10383453+rahul10992@users.noreply.github.com>
2024-07-04 18:01:50 -0700

    rectangle using EBO and glDrawElements

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ const char *fragmentShaderSource = "#version 330 core\n"
                                  "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
                                  "}\0";
 
-
+GLenum wireframe = GL_LINE;
 int main() {
     // glfw: initialize and configure
     // ------------------------------
@@ -110,13 +110,21 @@ int main() {
     glDeleteShader(fragmentShader);
 
     // vertices:
+//    float vertices[] = {
+//            -0.5f, -0.5f, 0.0f, // left
+//            0.5f, -0.5f, 0.0f, // right
+//            0.0f,  0.5f, 0.0f  // top
+//    };
     float vertices[] = {
-            -0.5f, -0.5f, 0.0f, // left
-            0.5f, -0.5f, 0.0f, // right
-            0.0f,  0.5f, 0.0f  // top
+            0.5f,  0.5f, 0.0f,  // top right
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left
     };
-
-
+    unsigned int indices[] = {  // note that we start from 0!
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
+    };
     // VAO
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
@@ -128,12 +136,21 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    // EBO
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     // render loop
     while(!glfwWindowShouldClose(window))
     {
+        // wireframe mode or fill mode
+        glPolygonMode(GL_FRONT_AND_BACK, wireframe);
+
         // input
         processInput(window);
 
@@ -145,7 +162,9 @@ int main() {
         // draw the first triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); // bind the VAO
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // poll all events and swap buffers
         glfwPollEvents();
@@ -153,6 +172,7 @@ int main() {
     }
 
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteVertexArrays(1, &VAO);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
@@ -167,6 +187,12 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 
 void processInput(GLFWwindow *window)
 {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
+        if(wireframe == GL_LINE)
+            wireframe = GL_FILL;
+        else
+            wireframe = GL_LINE;
+    }
+    else if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
